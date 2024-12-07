016f087ae57b82f8665c391d5bd6840554c9f43e
SP393 <sruthipandiath@gmail.com>
2024-12-06 19:30:29 -0500

    a

--- restaurant-app/server.js
+++ restaurant-app/server.js
@@ -56,11 +56,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: { secure: true }, // Set to true for HTTPS
+    cookieSession({
+        name: 'session',
+        keys: [process.env.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
     })
 );
 app.use(methodOverride('_method'));
